c447808d729e48c4426a377799c713949cd412e8
BalamIndira <balamindu2002@gmail.com>
2025-09-08 13:25:11 +0530

    todo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate, NavLink } from "react-router-dom";
 import { Login } from "./Login";
 import { Taskmanager } from "./Components/Taskmanager";
+import { Test } from "./Test";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -14,14 +15,18 @@ function App() {
 
   return (
     <div>
+      <nav>
+        <NavLink to={"/test"}>Test</NavLink>
+      </nav>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/test"element={<Test/>} />
         <Route path="/tasks" element={<Taskmanager/>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
 
-      {/* <h1>Simple task manager</h1>
-    <Taskmanager/> */}
+      <h1>Simple task manager</h1>
+    <Taskmanager/>
     </div>
   );
 }
